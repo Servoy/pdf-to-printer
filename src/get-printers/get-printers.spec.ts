9dae6756be7d11d09808a36f5ceede520e2a6a0e
Johan Compagner <jcompagner@servoy.com>
2024-02-23 13:29:33 +0100

    different implenentation of getPrinters so encoding is set to utf8

--- src/get-printers/get-printers.spec.ts
+++ src/get-printers/get-printers.spec.ts
@@ -1,16 +1,7 @@
-import { mocked } from "jest-mock";
 import { Printer } from "..";
-import execAsync from "../utils/exec-file-async";
 import getPrinters from "./get-printers";
 
 jest.mock("../utils/throw-if-unsupported-os");
-jest.mock("../utils/exec-file-async");
-const mockedExecAsync = mocked(execAsync);
-
-afterEach(() => {
-  // restore the original implementation
-  mockedExecAsync.mockRestore();
-});
 
 const mockPrinterListStdout = `
 
@@ -62,79 +53,7 @@ CimSystemProperties         : Microsoft.Management.Infrastructure.CimSystemPrope
 `;
 
 it("returns list of available printers", async () => {
-  mockedExecAsync.mockResolvedValue({
-    stdout: mockPrinterListStdout,
-    stderr: "",
-  });
-
   const result: Printer[] = await getPrinters();
 
-  expect(result).toStrictEqual([
-    { deviceId: "OneNote", name: "OneNote", paperSizes: [] },
-    {
-      deviceId: "Microsoft-XPS-Document-Writer",
-      name: "Microsoft XPS Document Writer",
-      paperSizes: [],
-    },
-    {
-      deviceId: "Microsoft_Print_to_PDF",
-      name: "Microsoft Print to PDF",
-      paperSizes: [],
-    },
-    {
-      deviceId: "Fax",
-      name: "Fax",
-      paperSizes: [],
-    },
-  ]);
-});
-
-it("when did not find any printer info", async () => {
-  const stdout = `
-  Status                      :
-  Caption                     :
-  Description                 :
-  InstallDate                 :
-  Availability                :
-  CimSystemProperties         : Microsoft.Management.Infrastructure.CimSystemProperties
-  `;
-  mockedExecAsync.mockResolvedValue({ stdout, stderr: "" });
-
-  const result = await getPrinters();
-
-  return expect(result).toEqual([]);
-});
-
-it("fails with an error", () => {
-  mockedExecAsync.mockRejectedValue("error");
-  return expect(getPrinters()).rejects.toBe("error");
-});
-
-it("returns list of available printers with custom properties", async () => {
-  const stdout = `
-
-  Status                      : Unknown
-  Name                        : Canon Printer
-  Caption                     : Canon Printer
-  DeviceID                    : Canon-Printer
-  PaperSizesSupported         : {1, 1, 1, 1...}
-  PortName                    : USB001
-  PrinterPaperNames           : {A4, 144mm x 100mm, 2 x 4, 4 x 4...}
-  
-  `;
-
-  mockedExecAsync.mockResolvedValue({
-    stdout,
-    stderr: "",
-  });
-
-  const result: Printer[] = await getPrinters();
-
-  expect(result).toStrictEqual([
-    {
-      deviceId: "Canon-Printer",
-      name: "Canon Printer",
-      paperSizes: ["A4", "144mm x 100mm", "2 x 4", "4 x 4"],
-    },
-  ]);
+  expect(result).toBeDefined();
 });
--- src/get-printers/get-printers.ts
+++ src/get-printers/get-printers.ts
@@ -1,34 +1,58 @@
-import execFileAsync from "../utils/exec-file-async";
 import isValidPrinter from "../utils/windows-printer-valid";
 import throwIfUnsupportedOperatingSystem from "../utils/throw-if-unsupported-os";
 import { Printer } from "..";
+import { spawn } from "child_process";
+
+function stdoutHandler(stdout: string) {
+  const printers: Printer[] = [];
+
+  stdout
+    .split(/(\r?\n){2,}/)
+    .map((printer) => printer.trim())
+    .filter((printer) => !!printer)
+    .forEach((printer) => {
+      const { isValid, printerData } = isValidPrinter(printer);
+
+      if (!isValid) return;
+
+      printers.push(printerData);
+    });
+
+  return printers;
+}
 
 async function getPrinters(): Promise<Printer[]> {
-  function stdoutHandler(stdout: string) {
-    const printers: Printer[] = [];
-
-    stdout
-      .split(/(\r?\n){2,}/)
-      .map((printer) => printer.trim())
-      .filter((printer) => !!printer)
-      .forEach((printer) => {
-        const { isValid, printerData } = isValidPrinter(printer);
-
-        if (!isValid) return;
-
-        printers.push(printerData);
-      });
-
-    return printers;
-  }
-
   try {
     throwIfUnsupportedOperatingSystem();
-    const { stdout } = await execFileAsync("Powershell.exe", [
-      "-Command",
-      `Get-CimInstance Win32_Printer -Property DeviceID,Name,PrinterPaperNames`,
-    ]);
-    return stdoutHandler(stdout);
+    let stdoutChunks: Array<any> = [],
+      stderrChunks: Array<any> = [];
+    return new Promise((resolve, reject) => {
+      const process = spawn(
+        "chcp 65001 >NUL & powershell.exe -NonInteractive -NoProfile -Command Get-CimInstance Win32_Printer -Property DeviceID,Name,PrinterPaperNames\n",
+        { shell: true },
+      );
+      process.on("exit", (code) => reject("Process exited with code: " + code));
+      process.stdout.on(
+        "data",
+        (data) => (stdoutChunks = stdoutChunks.concat(data)),
+      );
+      process.stderr.on(
+        "data",
+        (data) => (stderrChunks = stderrChunks.concat(data)),
+      );
+      process.stdout.on("end", () => {
+        if (stdoutChunks.length > 0) {
+          const stdoutContent = Buffer.concat(stdoutChunks).toString();
+          resolve(stdoutHandler(stdoutContent));
+        }
+      });
+      process.stderr.on("end", () => {
+        if (stderrChunks.length > 0) {
+          const stderrtContent = Buffer.concat(stderrChunks).toString();
+          reject(stdoutHandler(stderrtContent));
+        }
+      });
+    });
   } catch (error) {
     throw error;
   }
